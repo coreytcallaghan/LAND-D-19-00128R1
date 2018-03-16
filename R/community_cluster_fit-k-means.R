5d176c746dcaef5b12ef1baadad41f069b09897e
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2018-03-16 16:04:30 +1100

    more starts

--- R/community_cluster_fit-k-means.R
+++ R/community_cluster_fit-k-means.R
@@ -12,31 +12,36 @@ my_kmeans <- function(k, data, ...) {
   fm <- kmeans(data, k, ...)
   list(allocations = as.integer(fm$cluster),
        diagnostics = data.frame(total_ss = fm$totss,
-                                within_ss = fm$withinss,
+                                #within_ss = fm$withinss, # don't need for the meantime
                                 total_within_ss = fm$tot.withinss,
                                 between_ss = fm$betweenss,
                                 n_obs = length(fm$cluster),
                                 n_vars = ncol(fm$centers),
                                 k = nrow(fm$centers)))
-}  # this is really wasteful, as only within_ss changes each row, but meh, saves an unlist later
+}  # this is really wasteful if you want within_ss, as only within_ss changes each row, but meh, saves an unlist later
 
 # function to fit the kmeans to different matrices
 ## k_range is the range of cluster numbers to fir and test
 ## do.clustering is whether to actually perform the clustering (TRUE) or just check all data can be loaded and is numeric (FALSE)
-fit_kmeans <- function(file, k_range, do.clustering = TRUE, ...) {
+fit_kmeans <- function(file, k_range, min.checklists = 2, do.clustering = TRUE, ...) {
   message(paste0("Clustering ", file))
   # load and check data
   matrix <- get(load(paste0("Data/Matrix for each BCR/", file)))
   matrix <- matrix %>%
     ungroup() %>%
+    filter(total_checklists >= min.checklists)
+  localities <- matrix$LOCALITY_ID
+  matrix <- matrix %>%
     select(-LOCALITY_ID, -total_checklists, -URBAN_NONURBAN, 
-           -LC, -AGGREGATED_LANDCOVER, -NAME10)
-  #names(matrix) <- 1:ncol(matrix)
+           -LC, -AGGREGATED_LANDCOVER, -NAME10) %>%
+    select(which(colSums(.) > 0))
   if (any(colSums(matrix) == 0)) stop("There are columns with zero data")
+  #names(matrix) <- 1:ncol(matrix)
   # do the k-means
   if (do.clustering) {
     out <- lapply(k_range, my_kmeans, matrix, ...)
     names(out) <- k_range
+    out$localities <- localities
     out
   }
 }
@@ -52,7 +57,7 @@ fit_kmeans <- function(file, k_range, do.clustering = TRUE, ...) {
 
 files <- list.files("Data/Matrix for each BCR/")
 allocations <- mclapply(mc.cores = 16, 
-                        X = files, FUN = fit_kmeans, 2:100, iter.max = 30)
+                        X = files, FUN = fit_kmeans, 2:50, iter.max = 30, nstart = 20)
 names(allocations) <- gsub("_matrix.RData", "", files)
 
 saveRDS(allocations, file = "bcr_kmeans_allocations.rds")
