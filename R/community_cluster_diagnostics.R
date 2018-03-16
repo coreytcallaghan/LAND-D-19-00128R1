4035bf048cb07ce573bff3659ea1f59f4b5666cf
KITE\Mitch <mitchell.lyons@unsw.edu.au>
2018-03-16 16:05:05 +1100

    add per-cluster metrics, plus add functions file

--- R/community_cluster_diagnostics.R
+++ R/community_cluster_diagnostics.R
@@ -2,43 +2,30 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+source("R/community_cluster_diagnostics_funs.R")
+
 # load allocations calculate in community_cluster_fit-k-means.R
 allocations <- readRDS("Outputs/cluster_allocations/bcr_kmeans_allocations.rds")
+
+# load the localities that were used for clustering
+localities_list <- lapply(allocations, function(x) x[["localities"]])
 
 
 
 # inspect the sum of squares ----------------------------------------------
 
-
-# specific function to flatten the output list
-## this is a bit tricky
-## basically it unlists a couple times, adding in appropriate column names as needed
-flatten_list <- function(x) {
-  diagnostic_dfs <- lapply(x, `[[`, 2)
-  #diagnostic_dfs <- lapply(diagnostic_dfs, function(x){x$k = nrow(x); x})
-  bind_rows(diagnostic_dfs)
-}
+# flatten
 allocations_diagnostics <- bind_rows(lapply(allocations, flatten_list), .id = "BCR") # this is where the BCR unlisting is done
 
-# make a plot friendly output
-## now i wish i didn't make those data.frames - this is just to get the values that don't change for each cluster...
-allocations_diagnostics_summary <- allocations_diagnostics %>%
-  group_by(BCR, k) %>%
-  summarise(total_ss = mean(total_ss),
-            total_within_ss = mean(total_within_ss),
-            between_ss = mean(between_ss),
-            n_obs = mean(n_obs), # we need number of obs and species for AIC/BIC calculation
-            n_vars = mean(n_vars))
-
 # add information criterion
-allocations_diagnostics_summary <- allocations_diagnostics_summary %>%
+allocations_diagnostics_summary <- allocations_diagnostics %>%
   mutate(AIC = total_within_ss + (2 * n_vars * k),
          BIC = total_within_ss + (log(n_obs) * n_vars * k))
 
 # go long for plotting
 allocations_diagnostics_plot <- allocations_diagnostics_summary %>%
-  select(BCR, k, AIC, BIC) %>%
-  gather(metric, value, AIC:BIC)
+  select(BCR, k, total_within_ss, AIC, BIC) %>%
+  gather(metric, value, total_within_ss:BIC)
 
 ggplot(allocations_diagnostics_plot, aes(x = k, group = metric)) +
   geom_line(aes(y = value, colour = metric)) +
@@ -48,8 +35,12 @@ ggplot(filter(allocations_diagnostics_plot, metric == "AIC"), aes(x = k, group =
   geom_line(aes(y = value, colour = metric)) +
   facet_wrap(~BCR, scales = "free")
 
+ggplot(filter(allocations_diagnostics_plot, metric == "total_within_ss"), aes(x = k, group = metric)) +
+  geom_line(aes(y = value, colour = metric)) +
+  facet_wrap(~BCR, scales = "free")
+
 ### looks like AIC is more informative at this point
-### will try re-run with a distance metric on the data
+### will try re-run with a distance metric on the data?
 
 
 
@@ -59,76 +50,107 @@ min_AIC_data <- allocations_diagnostics_summary %>%
   group_by(BCR) %>%
   filter(AIC == min(AIC))
 
-
-# #### in next version, i've named the lists by k, so they can be directly indexed, i.e.
-# min_AIC_k <- min_AIC_data$k
-# retreive_k_clusters <- function(bcrs, ks) {
-#   bcrs[[as.character(ks)]][["allocations"]]
-# }
-# min_AIC_clusters <- purrr::map2(allocations, min_AIC_k, retreive_k_clusters)
-
-# #### for the mean time:
-min_AIC_k <- min_AIC_data$k - 1 # -1 because clustering was done k = 2:100
-retreive_k_clusters <- function(bcrs, ks) {
-  bcrs[[ks]][["allocations"]]
-}
-min_AIC_clusters <- purrr::map2(allocations, min_AIC_k, retreive_k_clusters)
-
+min_AIC_clusters <- purrr::map2(allocations, min_AIC_data$k, retreive_k_clusters)
 
 
 
 # compare communities to land cover classification ------------------------
 
 # join the data together
-join_lc_data <- function(x, clustering) {
-  matrix <- get(load(paste0("Data/Matrix for each BCR/", x, "_matrix.RData")))
-  #gsub("2", "URBAN", matrix$URBAN_NONURBAN) # check if correct
-  out <- data.frame(clusters = clustering[[x]],
-                    urban = matrix$URBAN_NONURBAN,
-                    landcover = matrix$AGGREGATED_LANDCOVER,
-                    stringsAsFactors = F)
-  filter(out, urban != "2") # for the meantime filter them out
-}
-
-clusters_landcover <- lapply(names(min_AIC_clusters), join_lc_data, min_AIC_clusters)
+clusters_landcover <- lapply(names(min_AIC_clusters), join_lc_data, min_AIC_clusters, localities_list)
 names(clusters_landcover) <- names(min_AIC_clusters)
 
 
-## calculate entropy
-# function to skip zeros
-log0 <- function(x) {
-  ifelse(x<=0, 0, log2(x))
-}
-# shannon entropy
-shannon_entropy <- function(x) {
-  -sum(x * log0(x))
-}
-# calculate entropy on xtab freqs
-calculate_entropy <- function(x, type = "urban") {
-  if (type == "urban") {
-    xtab <- prop.table(table(x$urban, x$clusters), margin = 1) # frequencies
-    as.data.frame(as.list(apply(xtab, 1, shannon_entropy)))
-  } else if (type == "landcover") {
-    xtab <- prop.table(table(x$landcover, x$clusters), margin = 1) # frequencies
-    as.data.frame(as.list(apply(xtab, 1, shannon_entropy)))
-  }
-}
+#### urban / non-urban comparison
 
-
-urban_entropy <- gather(bind_rows(lapply(clusters_landcover, calculate_entropy, type = "urban"), .id = "BCR"),
-                        "zone", "value", -BCR)
-landcover_entropy <- gather(bind_rows(lapply(clusters_landcover, calculate_entropy, type = "landcover"), .id = "BCR"),
-                            "zone", "value", -BCR)
-
+# entropy values - examine distriubtion of assembleges
+urban_entropy <- gather(bind_rows(lapply(clusters_landcover, calculate_entropy, type = "urban"),
+                                  .id = "BCR"), "zone", "value", -BCR)
 
 ggplot(urban_entropy, aes(y = value, x = zone)) +
   geom_boxplot() +
   geom_point(aes(colour = BCR)) +
   geom_line(aes(group = BCR))
 
-ggplot(landcover_entropy, aes(y = value)) +
-  geom_boxplot(aes(x = zone))
+summary(nlme::lme(fixed = value ~ zone, random = ~ 1 | BCR, data = urban_entropy))
 
+# urban_diff values - examine whether (proportionally) more of the communities are in non-urban areas
+urban_urbandiff <- bind_rows(lapply(clusters_landcover, calculate_urban_diff, type = "urban"),
+                             .id = "BCR")
+
+ggplot(urban_urbandiff, aes(y = urban_diff, x = BCR)) +
+  #geom_violin() +
+  geom_point(alpha = 0.5, size = 3) +
+  geom_hline(yintercept = 0, colour = "red") +
+  theme_classic()
+
+
+#### land cover comparison
+
+# entropy values - examine distriubtion of assembleges
+landcover_entropy <- gather(bind_rows(lapply(clusters_landcover, calculate_entropy, type = "landcover"), 
+                                      .id = "BCR"),
+                            "zone", "value", -BCR) %>%
+  filter(zone %in% c("Natural.Green.Area", "Urban.Green.Area"))
+
+ggplot(landcover_entropy, aes(y = value, x = zone)) +
+  geom_boxplot() +
+  geom_point(aes(colour = BCR)) +
+  geom_line(aes(group = BCR))
+
+summary(nlme::lme(fixed = value ~ zone, random = ~ 1 | BCR, data = landcover_entropy))
+
+# urban_diff values - examine whether (proportionally) more of the communities are in non-urban areas
+landcover_urbandiff <- bind_rows(lapply(clusters_landcover, calculate_urban_diff, type = "landcover"),
+                                 .id = "BCR")
+
+ggplot(landcover_urbandiff, aes(y = urban_diff, x = BCR)) +
+  #geom_violin() +
+  geom_point(alpha = 0.5, size = 3) +
+  geom_hline(yintercept = 0, colour = "red") +
+  theme_classic()
+
+
+
+
+
+# calculate per-cluster species metrics (i.e. community metrics) ------------------------
+
+# calculate metrics (richess, diversity etc.) given a clustering solution to index by
+metrics_per_cluster <- function(x, cluster_allocation, data) {
+  data <- data[which(cluster_allocation %in% x),] %>%
+    select(which(colSums(.) > 0))
+  data.frame(richness = ncol(data))
+}
+
+# subset to appropriate data and get metrics per cluster per bcr
+calculate_cluster_metrics <- function(x, clustering, localities) {
+  locality_df <- data.frame(LOCALITY_ID = localities[[x]], stringsAsFactors = F)
+  matrix <- get(load(paste0("Data/Matrix for each BCR/", x, "_matrix.RData")))
+  matrix <- matrix %>%
+    inner_join(locality_df, by = "LOCALITY_ID") %>%
+    ungroup() %>%
+    select(-(LOCALITY_ID:NAME10)) %>%
+    select(which(colSums(.) > 0))
+  bind_rows(lapply(unique(clustering[[x]]), metrics_per_cluster, clustering[[x]], matrix), .id = "cluster")
+}
+
+clusters_metrics <- lapply(names(min_AIC_clusters), calculate_cluster_metrics,
+                           min_AIC_clusters, localities_list)
+names(clusters_metrics) <- names(min_AIC_clusters)
+
+cluster_metrics_df <- bind_rows(clusters_metrics, .id = "BCR") %>%
+  mutate(cluster = as.integer(cluster))
+
+# join with other data...
+cluster_metrics_urbandiff <- inner_join(urban_urbandiff, cluster_metrics_df)
+cluster_metrics_lcdiff <- inner_join(landcover_urbandiff, cluster_metrics_df)
+# check for trends...
+
+
+
+plot(urban_diff~richness, data = cluster_metrics_urbandiff)
+plot(urban_diff~richness, data = cluster_metrics_lcdiff)
 
 
 
---
+++ R/community_cluster_diagnostics_funs.R
@@ -0,0 +1,60 @@
+# specific function to flatten the output list
+## this is a bit tricky
+## basically it unlists a couple times, adding in appropriate column names as needed
+flatten_list <- function(x) {
+  diagnostic_dfs <- lapply(head(x, -1), `[[`, 2) # use head() to ignore the localities vector
+  #diagnostic_dfs <- lapply(diagnostic_dfs, function(x){x$k = nrow(x); x})
+  bind_rows(diagnostic_dfs)
+}
+
+#### i've named the lists by k, so they can be directly indexed, i.e.
+retreive_k_clusters <- function(bcrs, ks) {
+  bcrs[[as.character(ks)]][["allocations"]]
+}
+
+# function to join data on
+join_lc_data <- function(x, clustering, localities) {
+  locality_df <- data.frame(LOCALITY_ID = localities[[x]], stringsAsFactors = F)
+  matrix <- get(load(paste0("Data/Matrix for each BCR/", x, "_matrix.RData")))
+  matrix <- inner_join(matrix, locality_df, by = "LOCALITY_ID")
+  #gsub("2", "URBAN", matrix$URBAN_NONURBAN) # check if correct
+  out <- data.frame(clusters = clustering[[x]],
+                    urban = matrix$URBAN_NONURBAN,
+                    landcover = matrix$AGGREGATED_LANDCOVER,
+                    stringsAsFactors = F)
+  filter(out, urban != "2") # filter out 2's if they exist
+}
+
+## calculate entropy
+# function to skip zeros
+log0 <- function(x) {
+  ifelse(x<=0, 0, log2(x))
+}
+# shannon entropy
+shannon_entropy <- function(x) {
+  -sum(x * log0(x))
+}
+# calculate entropy on xtab freqs
+calculate_entropy <- function(x, type = "urban") {
+  if (type == "urban") {
+    xtab <- prop.table(table(x$urban, x$clusters), margin = 1) # frequencies
+    as.data.frame(as.list(apply(xtab, 1, shannon_entropy)))
+  } else if (type == "landcover") {
+    xtab <- prop.table(table(x$landcover, x$clusters), margin = 1) # frequencies
+    as.data.frame(as.list(apply(xtab, 1, shannon_entropy)))
+  }
+}
+
+## calculate membership disparity
+## positive values will mean more proportional membership of sites to the non-urban component
+calculate_urban_diff <- function(x, type = "urban") {
+  if (type == "urban") {
+    xtab <- prop.table(table(x$urban, x$clusters), margin = 1) # frequencies
+    data.frame(urban_diff = xtab["NONURBAN",] - xtab["URBAN",],
+    cluster = 1:ncol(xtab))
+  } else if (type == "landcover") {
+    xtab <- prop.table(table(x$landcover, x$clusters), margin = 1) # frequencies
+    data.frame(urban_diff = xtab["Natural Green Area",] - xtab["Urban Green Area",],
+               cluster = 1:ncol(xtab))
+  }
+}
