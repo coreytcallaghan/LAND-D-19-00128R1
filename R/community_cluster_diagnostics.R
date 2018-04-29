938d18aba00f3361967894aaa38b41e5db124750
Corey Callaghan <callaghan.corey.t@gmail.com>
2018-04-30 08:29:00 +1000

    making figures and doing t-test for paper

--- R/community_cluster_diagnostics.R
+++ R/community_cluster_diagnostics.R
@@ -31,9 +31,23 @@ ggplot(allocations_diagnostics_plot, aes(x = k, group = metric)) +
   geom_line(aes(y = value, colour = metric)) +
   facet_wrap(~BCR, scales = "free")
 
-ggplot(filter(allocations_diagnostics_plot, metric == "AIC"), aes(x = k, group = metric)) +
+allocations_diagnostics_plot %>%
+  filter(metric == "AIC") %>%
+  mutate(BCR = gsub("_", " ", .$BCR)) %>%
+  mutate(BCR = gsub("WEST GULF COASTAL PLAIN OUACHITAS", "WEST GULF COASTAL PLAIN/OUACHITAS", .$BCR)) %>%
+  mutate(BCR = gsub("SOUTHERN ROCKIES COLORADO PLATEAU", "SOUTHERN ROCKIES/COLORADO PLATEAU", .$BCR)) %>%
+  mutate(BCR = gsub("LOWER GREAT LAKES ST LAWRENCE PLAIN", "LOWER GREAT LAKES/ST. LAWRENCE PLAIN", .$BCR)) %>%
+  mutate(BCR = gsub("NEW ENGLAND MID ATLANTIC COAST", "NEW ENGLAND/MID-ATLANTIC COAST", .$BCR)) %>%
+ggplot(., aes(x = k, group = metric)) +
   geom_line(aes(y = value, colour = metric)) +
-  facet_wrap(~BCR, scales = "free")
+  facet_wrap(~BCR, scales = "free", labeller = labeller(BCR = label_wrap_gen(18)))+
+  xlab("k")+
+  ylab("value")+
+  guides(color=FALSE)
+
+ggsave(file="H:/Dissertation/Dissertation Chapters/Data Chapters/United States Urban Bird Patterns/Appendices/Appendix 3/Appendix_3a.png",
+       width=480, height=320, units="mm", dpi=300)
+
 
 ggplot(filter(allocations_diagnostics_plot, metric == "total_within_ss"), aes(x = k, group = metric)) +
   geom_line(aes(y = value, colour = metric)) +
@@ -98,16 +112,49 @@ landcover_entropy <- gather(bind_rows(lapply(clusters_landcover, calculate_entro
                             "zone", "value", -BCR) %>%
   filter(zone %in% c("Natural.Green.Area", "Urban.Green.Area")) %>%
   mutate(zone = gsub("Natural.Green.Area", "Natural Green Area", .$zone)) %>%
-  mutate(zone = gsub("Urban.Green.Area", "Urban Green Area", .$zone))
+  mutate(zone = gsub("Urban.Green.Area", "Urban Green Area", .$zone)) %>%
+  mutate(BCR = gsub("_", " ", .$BCR)) %>%
+  mutate(BCR = gsub("WEST GULF COASTAL PLAIN OUACHITAS", "WEST GULF COASTAL PLAIN/OUACHITAS", .$BCR)) %>%
+  mutate(BCR = gsub("SOUTHERN ROCKIES COLORADO PLATEAU", "SOUTHERN ROCKIES/COLORADO PLATEAU", .$BCR)) %>%
+  mutate(BCR = gsub("LOWER GREAT LAKES ST LAWRENCE PLAIN", "LOWER GREAT LAKES/ST. LAWRENCE PLAIN", .$BCR)) %>%
+  mutate(BCR = gsub("NEW ENGLAND MID ATLANTIC COAST", "NEW ENGLAND/MID-ATLANTIC COAST", .$BCR))
 
 ggplot(landcover_entropy, aes(y = value, x = zone)) +
   geom_violin(fill="azure3") +
   geom_point(aes(colour = BCR)) +
-  geom_line(aes(group = BCR)) +
-  theme_classic() +
-  xlab("") +
-  ylab("Shannon diversity of cluster assignments") +
-  coord_flip()
+  geom_line(aes(group = BCR, colour = BCR)) +
+  stat_summary(fun.y=mean, geom="point", size=6, shape=15, alpha=0.7, color="red")+
+  theme_classic()+
+  ylab("Shannon diversity of cluster assignments")+
+  xlab("Aggregated landcover")+
+  coord_flip()+
+  theme(axis.text.x=element_text(size=16))+
+  theme(axis.text.y=element_text(size=16))+
+  theme(axis.title.x = element_text(size=20))+
+  theme(axis.title.y=element_text(size=20))
+
+ggsave(file="H:/Dissertation/Dissertation Chapters/Data Chapters/United States Urban Bird Patterns/Figures/Figure 6/Cluster_assignments.png",
+       width=480, height=300, units="mm", dpi=600)
+
+
+## stats to report for paper
+landcover_entropy %>%
+  group_by(zone) %>%
+  summarise(mean=mean(value),
+            sd=sd(value))
+
+urban <- landcover_entropy %>%
+  filter(zone == "Urban Green Area") %>%
+  .$value
+
+natural <- landcover_entropy %>%
+  filter(zone == "Natural Green Area") %>%
+  .$value
+
+
+var.test(urban, natural) 
+
+t.test(urban, natural, var.equal=FALSE, paired=FALSE)
 
 summary(nlme::lme(fixed = value ~ zone, random = ~ 1 | BCR, data = landcover_entropy))
 
@@ -115,13 +162,27 @@ summary(nlme::lme(fixed = value ~ zone, random = ~ 1 | BCR, data = landcover_ent
 landcover_urbandiff <- bind_rows(lapply(clusters_landcover, calculate_urban_diff, type = "landcover"),
                                  .id = "BCR")
 
-ggplot(landcover_urbandiff, aes(y = urban_diff, x = BCR)) +
-  #geom_violin() +
+library(forcats)
+landcover_urbandiff %>%
+  group_by(BCR) %>%
+  summarise(mean=mean(urban_diff)) %>%
+  inner_join(., landcover_urbandiff, by="BCR") %>%
+  arrange(mean) %>%
+  mutate(BCR = gsub("_", " ", .$BCR)) %>%
+  mutate(BCR = gsub("WEST GULF COASTAL PLAIN OUACHITAS", "WEST GULF COASTAL PLAIN/OUACHITAS", .$BCR)) %>%
+  mutate(BCR = gsub("SOUTHERN ROCKIES COLORADO PLATEAU", "SOUTHERN ROCKIES/COLORADO PLATEAU", .$BCR)) %>%
+  mutate(BCR = gsub("LOWER GREAT LAKES ST LAWRENCE PLAIN", "LOWER GREAT LAKES/ST. LAWRENCE PLAIN", .$BCR)) %>%
+  mutate(BCR = gsub("NEW ENGLAND MID ATLANTIC COAST", "NEW ENGLAND/MID-ATLANTIC COAST", .$BCR)) %>%
+ggplot(., aes(y = urban_diff, x = fct_inorder(BCR))) +
   geom_point(alpha = 0.5, size = 3) +
   geom_hline(yintercept = 0, colour = "red") +
-  theme_classic()
+  theme_classic()+
+  coord_flip()+
+  ylab("Proportional difference")+
+  xlab("BCR")
 
-
+ggsave(file="H:/Dissertation/Dissertation Chapters/Data Chapters/United States Urban Bird Patterns/Appendices/Appendix 3/Appendix_3b.png",
+       width=420, height=320, units="mm", dpi=300)
 
 
 
@@ -147,9 +208,22 @@ plot(urban_diff~diversity, data = cluster_metrics_urbandiff)
 plot(urban_diff~diversity, data = cluster_metrics_lcdiff)
 
 
+## Make two plots for appendix for paper
+ggplot(cluster_metrics_lcdiff, aes(x=urban_diff, y=richness))+
+  geom_point()+
+  theme_classic()+
+  xlab("Proportional difference")+
+  ylab("Total richness")
 
 
+ggsave(file="H:/Dissertation/Dissertation Chapters/Data Chapters/United States Urban Bird Patterns/Appendices/Appendix 3/Appendix_3c.png",
+       width=350, height=280, units="mm", dpi=300)
 
+ggplot(cluster_metrics_lcdiff, aes(x=urban_diff, y=diversity))+
+  geom_point()+
+  theme_classic()+
+  xlab("Proportional difference")+
+  ylab("Total diversity")
 
 
 
