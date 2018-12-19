5e8607cf387bf98a80990689b5e3223344074d35
Mitchell Lyons <mitchell.lyons@unsw.edu.au>
2018-12-19 23:46:50 +1100

    change to heterogeneity metrics; add fix for points that fall outside raster extent

--- earth_engine/extract_lc_stats.js
+++ earth_engine/extract_lc_stats.js
@@ -30,34 +30,46 @@ var add_lc_properties = function(f) {
     maxPixels: 1
   }).get("lcagg")
   
-  // heterogeneity in aggregated class
-  var lcagg_var = landcover2011_agg.reduceRegion({
-    reducer: ee.Reducer.countDistinct(),
-    geometry: geom.buffer(500),
-    scale: 30,
-    maxPixels: 5000
-  }).get("lcagg")
+  if (lcagg > 0) {
   
-  // heterogeneity in original classes
-  var lc_var = landcover2011.reduceRegion({
-    reducer: ee.Reducer.countDistinct(),
-    geometry: geom.buffer(5000),
-    scale: 30,
-    maxPixels: 300000,
-    bestEffort: true
-  }).get("lc")
-  
-  var dict = ee.Dictionary({
-    lc_agg: lcagg,
-    lcagg_var: lcagg_var,
-    lc_var: lc_var
-  })
-  
-  return(f.set(dict))
+    // heterogeneity in aggregated class
+    var lcagg_hist = ee.Dictionary(
+      landcover2011_agg.reduceRegion({
+        reducer: ee.Reducer.frequencyHistogram(),
+        geometry: geom.buffer(500),
+        scale: 30,
+        maxPixels: 5000
+      }).get("lcagg")
+    )
+    
+    var sum = lcagg_hist.toArray().reduce(ee.Reducer.sum(), [0]).get([0])
+    var percs = lcagg_hist.map(function(k,v){return(ee.Number(v).divide(sum).multiply(100))})
+    var lcagg_pc = ee.Number(percs.get(lcagg)).toInt()
+    
+    // heterogeneity in original classes
+    var lc_hist = ee.Dictionary(
+      landcover2011.reduceRegion({
+        reducer: ee.Reducer.frequencyHistogram(),
+        geometry: geom.buffer(5000),
+        scale: 30,
+        maxPixels: 300000,
+        bestEffort: true
+      }).get("lc")
+    )
+    
+    var lchist_arr = lc_hist.toArray()
+    var p_perc = lchist_arr.divide(lchist_arr.reduce(ee.Reducer.sum(), [0]).get([0]))
+    var p_ent = p_perc.multiply(p_perc.log()).multiply(-1).reduce(ee.Reducer.sum(), [0]).get([0])
+    
+    return(f.set({lcagg: lcagg, lcagg_pc: lcagg_pc, lc_ent: p_ent}))
+  } else {
+    return(f.set({lcagg: -1, lcagg_pc: -1, lc_ent: -1}))
+  }
+
 }
 
 var checklists_info = checklists.map(add_lc_properties)
-print(checklists_info.first())
+print(checklists_info.limit(1000))
 
 Export.table.toDrive({
   collection: checklists_info,
